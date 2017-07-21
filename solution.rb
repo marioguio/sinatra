6762c6455aeb5cd2c751c9cef287e07e9d0a74b5
mario <mario_guio@outlook.com>
2017-07-21 18:56:54 -0500

    cambiando detalles

--- solution.rb
+++ solution.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get "/" do
   if params.has_key? :nombre || params[:nombre] != ""
-    nombre = params[:nombre].capitalize
+    nombre = params[:nombre]
     "<h1>Hola #{nombre}!<h1>"
   else
     "<h1>Hola desconocido!<h1>"
