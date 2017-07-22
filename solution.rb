36595f2fea152dac90c443a97de85e2bda24541b
mario <mario_guio@outlook.com>
2017-07-21 19:01:00 -0500

    cambiando detalles

--- solution.rb
+++ solution.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 
-get "/" do
+get "/makers/:nombre" do
   if params.has_key? :nombre || params[:nombre] != ""
-    nombre = params[:nombre]
+    nombre = params[:nombre].capitalize
     "<h1>Hola #{nombre}!<h1>"
   else
     "<h1>Hola desconocido!<h1>"
